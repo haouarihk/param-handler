f31dae7d223d54a59e9eeb0dc19da4de13c2a7b6
Haouari haithem <57036855+wolfpat01@users.noreply.github.com>
2021-02-28 13:13:07 +0100

    .

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -8,7 +8,7 @@ interface Init {
 /** this is the class that is gonna make your life 1% easier, if you're into classes
  * @param window is the window from your virtual dom
 */
-export declare class QParamer {
+export default class PH {
     window: Window;
     private _params;
     /** this is the function that is responsible of getting the queryString
@@ -30,8 +30,11 @@ export declare class QParamer {
     set(name: string, value: string): void;
     /** this function gets a variable value
      * @param name is the name of the variable that you wanna get its value
+     * @param dif is the default value when its not set
+     * @param reverse this will be used to reverse the answer only when its a boolean
+     *
      */
-    get(name: string): any;
+    get(name: string, dif?: any, reverse?: boolean): any;
     /** this function checks if a variable exists
      * @param name the name of the variable that you wanna check if it exists
     */
@@ -50,4 +53,6 @@ export declare function ParamsHandler(window: Window): Init;
 export declare function locationToObj(searchstr: string): any;
 export declare function ObjTolocation(obj: any): string;
 export declare function triggerEvent(document: Document, el: any, type: string): void;
+/** handles undefenition */
+export declare function valueHandler(prm: any, oud?: any, reverse?: boolean): any;
 export {};
